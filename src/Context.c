892be80251824fde3a14f0662eecdb84c9455305
dpi <desengge@163.com>
2015-07-08 21:57:19 +0800

    cpu100%

--- src/Context.c
+++ src/Context.c
@@ -20,6 +20,8 @@
 #include "Context.h"
 
 pcap_t * createDevice(const char * device) {
+	int ret;
+		
 	if (!device || strlen(device) == 0) return NULL;
 	
 	char errBuf[PCAP_ERRBUF_SIZE];
@@ -29,17 +31,44 @@ pcap_t * createDevice(const char * device) {
 	pcap_t * pcap = pcap_create(device, NULL);
 
 	if(pcap) {
-		printf("pcap_set_snaplen %d\n", pcap_set_snaplen(pcap, 65535));
-		printf("pcap_set_promisc %d\n", pcap_set_promisc(pcap, gCapturePromisc));
+		//printf("pcap_set_snaplen %d\n", pcap_set_snaplen(pcap, 65535));
+		//printf("pcap_set_promisc %d\n", pcap_set_promisc(pcap, gCapturePromisc));
 //		int	pcap_can_set_rfmon(pcap_t *);
 //		int	pcap_set_rfmon(pcap_t *, int);
-		printf("pcap_set_timeout %d\n", pcap_set_timeout(pcap, gCaptureTimeout));
+		//printf("pcap_set_timeout %d\n", pcap_set_timeout(pcap, gCaptureTimeout));
 //		pcap_set_tstamp_type(pcap_t *, int);
 //		pcap_set_immediate_mode(pcap_t *, int);
-		printf("pcap_set_buffer_size %d\n", pcap_set_buffer_size(pcap, gCaptureBuffer));
+		//printf("pcap_set_buffer_size %d\n", pcap_set_buffer_size(pcap, gCaptureBuffer));
 //		printf("pcap_set_tstamp_precision %d\n", pcap_set_tstamp_precision(pcap, PCAP_TSTAMP_PRECISION_NANO));
 
-		printf("pcap_activate %d\n", pcap_activate(pcap));
+		//printf("pcap_activate %d\n", pcap_activate(pcap));
+		ret = pcap_set_snaplen(pcap, 102400);
+        if (ret != 0) {
+            printf("You have an old version of libpcap:%s\n", pcap_statustostr(ret));
+            return NULL;
+        }
+        ret = pcap_set_promisc(pcap, 1);
+        if (ret != 0) {
+            printf("You have an old version of libpcap\n");
+            return NULL;
+        }
+        ret = pcap_set_timeout(pcap, 0);
+        if (ret != 0) {
+            printf("You have an old version of libpcap\n");
+            return NULL;
+        }
+        /* set capture buffer size to 16 MB */
+        ret = pcap_set_buffer_size(pcap, (1<<24));
+        if (ret != 0) {
+            printf("You have an old version of libpcap\n");
+            return NULL;
+        }
+        ret = pcap_activate(pcap);
+        if (ret != 0) {
+            printf("pcap_activate failed '%s'\n", pcap_geterr(pcap));
+            return NULL;
+        }
+
 
 //		pcap_set_snaplen(pcap, 65535);
 //		pcap_set_promisc(pcap, gCapturePromisc);
@@ -52,12 +81,13 @@ pcap_t * createDevice(const char * device) {
 //		struct bpf_program filter;
 //		printf("pcap_compile: %d\n", pcap_compile(pcap, &filter, "host 192.168.3.129", 0, mask));
 //		printf("pcap_setfilter: %d\n", pcap_setfilter(pcap, &filter));
-	} else {
+	} 
+#endif
+
+	if (!pcap) {
 		printf("error: pcap_open_live(): %s\n", errBuf);
 		return NULL;
 	}
-#endif
-
 
 	return pcap;
 }
